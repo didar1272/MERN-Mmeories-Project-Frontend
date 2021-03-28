ab8a99443e7669f4f69822254846893f5fd9289e
Md. Didarul Islam <reurnzero101@gmail.com>
2021-03-28 15:24:06 +0600

    Initial Form and Post done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import { useDispatch } from 'react-redux'; // it is used to dispatch an action
 
-
+import { getPosts } from "./actions/posts";
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
 import memories from './images/shining-circle.png';
@@ -10,6 +11,11 @@ import useStyles from './styles';
 
 const App = () => {
     const classes = useStyles();
+    const dispatch = useDispatch(); //this is a hook
+
+    useEffect(() => {
+        dispatch(getPosts());
+    }, [dispatch])
 
     return (
         <Container maxidth='lg'>
