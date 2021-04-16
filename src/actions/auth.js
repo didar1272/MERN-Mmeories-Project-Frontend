1e670d273578aecbc53488a9adbe147da6c9fd0d
Md. Didarul Islam <reurnzero101@gmail.com>
2021-04-16 22:34:33 +0600

    JWT Login

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,16 +1,23 @@
 import { AUTH } from "../constants/actionTypes";
-import * as api from '../api/index.js';
+import * as api from '../api/index';
 
-export const signin = (fromData, history) => async (dispatch) => {
+export const signin = (formData, history) => async (dispatch) => {
     try {
+        const { data } = await api.signIn(formData);
+
+        dispatch({type: AUTH, data });
+
         history.push('/');
     } catch (error) {
         console.log(error);
     }
 }
 
-export const signup = (fromData, history) => async (dispatch) => {
+export const signup = (formData, history) => async (dispatch) => {
     try {
+        const { data } = await api.signUp(formData);
+
+        dispatch({ type: AUTH, data });
         history.push('/');
     } catch (error) {
         console.log(error);
--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -34,7 +34,7 @@ const Auth = () => {
 
     const switchMode = () => {
         setIsSignup((prevIsSignup) => !prevIsSignup);
-        handleShowPassword(false);
+        setShowPassword(false);
     }
 
     const handleChange = (e) => {
