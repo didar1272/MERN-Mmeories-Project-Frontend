de6d954e0cd048968795adf7cf2c5b65c59343e9
Md. Didarul Islam <reurnzero101@gmail.com>
2021-04-17 10:45:21 +0600

    User Action

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -11,11 +11,12 @@ import useStyles from './styles';
 const Form = ({ currentId, setCurrentId}) => { 
 
     const [postData, setPostData] = useState({
-        creator: '', title: '', message: '', tags: '', selectedFile: ''
+        title: '', message: '', tags: '', selectedFile: ''
     });
     const post = useSelector((state) => currentId ? state.posts.find((p) => p._id === currentId) : null);
     const dispatch = useDispatch();
     const classes = useStyles();
+    const user = JSON.parse(localStorage.getItem('profile'));
 
     useEffect(() => {
         if(post) setPostData(post);
@@ -23,29 +24,37 @@ const Form = ({ currentId, setCurrentId}) => {
 
     const clear = () => {
         setCurrentId(null);
-        setPostData({ creator: '', title: '', message: '', tags: '', selectedFile: '' });
+        setPostData({ title: '', message: '', tags: '', selectedFile: '' });
     }
     
     const handleSubmit = async (e) => {
         e.preventDefault();
 
         if(currentId) {
-            dispatch(updatePost(currentId, postData));
+            dispatch(updatePost(currentId, {...postData, name: user?.result?.name}));
             
         }else {
-            dispatch(createPost(postData));
+            dispatch(createPost({...postData, name: user?.result?.name}));
         }
         clear();
         
     }
 
+    if(!user?.result?.name) {
+        return (
+            <Paper className={classes.paper}>
+                <Typography variant='h6' align='center'>
+                    Please Sign In to create your own memories and like other's memories.
+                </Typography>
+            </Paper>
+        );
+    }
+
+    
     return (
         <Paper className={classes.paper}>
             <form autoComplete="off" onValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
                 <Typography variant="h6">{ currentId ? "Editing" : "Creating" } a Memory</Typography>
-                <TextField name="creator" variant="outlined" label="Creator" fullWidth value={postData.creator}
-                    onChange={(e) => setPostData({ ...postData, creator: e.target.value })}
-                />
                 <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title}
                     onChange={(e) => setPostData({ ...postData, title: e.target.value })}
                 />
